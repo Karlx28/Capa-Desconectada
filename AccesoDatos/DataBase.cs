c2b54a5bdbebc391e19e06f43ed4afafd386bbfb
Karlx28 <mo0436032023@unab.edu.sv>
2024-09-28 10:25:30 -0600

    Guia XX

---
+++ AccesoDatos/DataBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    internal class DataBase
+    {
+        public static string ConnectcionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["NWConnection"].ConnectionString;
+            }
+        }
+
+        public static SqlConnection GetSqlConnection()
+        {
+            SqlConnection conexion = new SqlConnection(ConnectcionString);
+            conexion.Open();
+            return conexion;
+        }
+    }
+}
